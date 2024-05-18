cd8deca6d1e25149414b4a3337cd02fa65f2bd91
Reijer Boodt <reijerboodt@gmail.com>
2024-05-19 00:48:21 +0200

    Quick commit

--- main.c
+++ main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(int nargs, char **args) {
+typedef struct Body {
+    // Name
+    double mass;
+    double r_orbit;
+    double semi_major_axis;
+    double semi_minor_axis;
+
+} Body;
+
+int main (int argc, char *argv[]) {
     printf("hoer\n");
+    Body earth = {10,0,0,0};
     return 0;
 }
