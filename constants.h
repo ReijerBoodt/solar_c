d0a550428896e9d4257d3b5cf2eec7151592af7d
NaN-Hempel <nanne.hempel@gmail.com>
2024-05-19 16:53:06 +0200

    bodies.h

--- constants.h
+++ constants.h
@@ -4,4 +4,5 @@ static float G = 6.67408E-11;
 static float M_earth = 5.9722E24;
 static float M_moon = 7.342E22;
 static float lunar_distance = 3.84399E8;
+static float AU = 1.496E11;
 #endif // CONSTANTS
