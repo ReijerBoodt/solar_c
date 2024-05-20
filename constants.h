fec8f50f0daa6bd96ab272991e997da4a857f3b2
Reijer Boodt <reijerboodt@gmail.com>
2024-05-20 23:01:53 +0200

    Add more constants

--- constants.h
+++ constants.h
@@ -1,7 +1,10 @@
 #ifndef CONSTANTS
 #define CONSTANTS
-static float G = 6.67408E-11;
-static float M_earth = 5.9722E24;
-static float M_moon = 7.342E22;
-static float lunar_distance = 3.84399E8;
+static double G = 6.67408E-11;
+static double M_sun = 1.989E30;
+static double M_jupiter = 1.898E27;
+static double M_earth = 5.9722E24;
+static double M_moon = 7.342E22;
+static double AU = 149597870700.f / 2;
+static double lunar_distance = 3.84399E8;
 #endif // CONSTANTS
